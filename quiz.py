d44f012ca8899a3e541fde7438794476fb4a8e2a
Fulvio Corno <fulvio.corno@polito.it>
2017-11-29 22:07:27 +0100

    restructure function verify()

--- quiz.py
+++ quiz.py
@@ -2,17 +2,29 @@ __author__ = 'Fulvio Corno'
 
 
 def verify(v):
-    if (10 * v['a'] + v['b']) * (10 * v['b'] + v['c']) != 100 * v['d'] + 10 * v['e'] + v['f']:
+
+    n11 = v['b']
+    n12 = (10*v['a']+v['i'])
+    n13 = 100 * v['e'] + 10 * v['g'] + v['f']
+    n21 = 10*v['e']+v['f']
+    n22 = 10*v['i']+v['d']
+    n23 = 10 * v['a'] + v['f']
+    n31 = 100*v['i']+10*v['e']+v['h']
+    n32 = 10*v['b']+v['i']
+    n33 = 100*v['e']+10*v['c']+v['d']
+
+    if n11 * n12 != n13:
         return False
-    if (10 * v['a'] + v['g']) - (10 * v['a'] + v['a']) != v['h']:
+    if n21 + n22 != n23:
         return False
-    if (10 * v['c'] + v['f']) + (10 * v['i'] + v['b']) != (10 * v['g'] + v['d']):
+    if n31 + n32 != n33:
         return False
-    if (10 * v['a'] + v['b']) + (10 * v['a'] + v['g']) != (10 * v['c'] + v['f']):
+
+    if n11 * n21 != n31:
         return False
-    if (10 * v['b'] + v['c']) + (10 * v['a'] + v['a']) != (10 * v['i'] + v['b']):
+    if n12 + n22 != n32:
         return False
-    if (100 * v['d'] + 10 * v['e'] + v['f']) / (v['h']) != (10 * v['g'] + v['d']):
+    if n13 - n23 != n33:
         return False
 
     return True
